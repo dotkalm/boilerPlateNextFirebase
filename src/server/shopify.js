598612a5734c0b1017b238a424d85e370b9d4a97
joel holmberg <joelmholmberg@gmail.com>
2021-04-17 11:47:18 -0700

    next js configvarS

--- src/server/shopify.js
+++ src/server/shopify.js
@@ -8,9 +8,9 @@ Shopify.Context.initialize({
 	API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
 	SCOPES: process.env.SHOPIFY_API_SCOPES.split(","),
 	HOST_NAME: process.env.SHOPIFY_APP_URL.replace(/https:\/\//, ""),
-		API_VERSION: ApiVersion.October20,
-		IS_EMBEDDED_APP: true,
-		SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
+	API_VERSION: ApiVersion.October20,
+	IS_EMBEDDED_APP: true,
+	SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
 	})
 
 
