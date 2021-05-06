25104312a6b91d6488bbf6b71f663ff89023ecc4
joel holmberg <joelmholmberg@gmail.com>
2021-05-06 13:34:54 -0700

    remove firebase, psql

--- src/pages/api/graphql.js
+++ src/pages/api/graphql.js
@@ -1,19 +1,6 @@
 import { graphqlHTTP } from 'express-graphql'
 import Schema from '../../graphql/schema'
-import pool from '../../server/psql'
-let db
 
-const root = {
-	db: req => {
-		if(db === undefined){
-			db = pool
-			return db 
-		}else{
-			return db 
-		}
-	},
-}
- 
 const graphQLConfigs = {
 	schema: Schema,
 	rootValue: root,
