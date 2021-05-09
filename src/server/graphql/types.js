d8d2f1027ffbacebe35d3638984292d550d9c51b
joel holmberg <joelmholmberg@gmail.com>
2021-05-09 16:02:28 -0700

    node defs

--- src/server/graphql/types.js
+++ src/server/graphql/types.js
@@ -1,5 +1,9 @@
 import * as graphql from 'graphql'
-
+import { 
+	fromGlobalId, 
+	globalIdField, 
+	nodeDefinitions 
+} from '../node'
 const {
 	GraphQLList,
 	GraphQLString,
@@ -8,8 +12,29 @@ const {
 	GraphQLID,
 } = graphql
 
+export const { nodeField, nodeInterface } = nodeDefinitions(
+  globalId => {
+    const { type, id } = fromGlobalId(globalId)
+    switch (type) {
+      case 'Region':
+        return regionData.find((obj) => obj.id === id)
+      case 'Country':
+        return countryData.find((obj) => obj.id === id)
+      case 'Marina':
+        return countryData.find((obj) => obj.id === id)
+      case 'Base':
+        return baseData.find((obj) => obj.id === id)
+    }
+  },
+  obj => { 
+		if (obj.name) {
+      return userType
+		}
+  }
+)
 export const DestinationConnection = new GraphQLObjectType({
-	name: 'vesselDestinationOrRegionConnection',
+	name: 'VesselDestinationOrRegionConnection',
+	interfaces: [ nodeInterface ],
 	description: 'region of vessel connection',
 	fields: () => ({
 		edges: { 
@@ -20,10 +45,12 @@ export const DestinationConnection = new GraphQLObjectType({
 		},
 	})
 })
-export const DestinationType = new GraphQLObjectType({
-	name: 'vesselDestinationOrRegion',
-	description: 'region of vessel',
+export const RegionType = new GraphQLObjectType({
+	name: 'Region',
+	interfaces: [ nodeInterface ],
+	description: 'region of the world',
 	fields: () => ({
+		id: globalIdField(),
 		name : { 
 			type: GraphQLString, 
 			resolve: (p,args) => {
@@ -35,27 +62,33 @@ export const DestinationType = new GraphQLObjectType({
 	})
 })
 export const CountryType = new GraphQLObjectType({
-	name: 'vesselCountry',
+	name: 'Country',
+	interfaces: [ nodeInterface ],
 	description: 'country of vessel',
 	fields: () => ({
+		id: globalIdField(),
 		name : { type: GraphQLString },
 		apiServiceId: { type: GraphQLID },
 		apiService: { type: GraphQLString },
 	})
 })
 export const BaseType = new GraphQLObjectType({
-	name: 'vesselBase',
+	name: 'Base',
+	interfaces: [ nodeInterface ],
 	description: 'base of vessel',
 	fields: () => ({
+		id: globalIdField(),
 		name : { type: GraphQLString },
 		apiServiceId: { type: GraphQLID },
 		apiService: { type: GraphQLString },
 	})
 })
 export const MarinaType = new GraphQLObjectType({
-	name: 'vesselMarina',
+	name: 'Marina',
+	interfaces: [ nodeInterface ],
 	description: 'marina of vessel',
 	fields: () => ({
+		id: globalIdField(),
 		name : { type: GraphQLString },
 		apiServiceId: { type: GraphQLID },
 		apiService: { type: GraphQLString },
