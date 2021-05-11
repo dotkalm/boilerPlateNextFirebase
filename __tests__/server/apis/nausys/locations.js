a8432263649a66e13bce04822d06ee1464b7c7ba
joel holmberg <joelmholmberg@gmail.com>
2021-05-11 13:17:22 -0700

    geohash 6 to sync w other vendors

--- __tests__/server/apis/nausys/locations.js
+++ __tests__/server/apis/nausys/locations.js
@@ -1,5 +1,6 @@
 import { getNausys } from '../../../../src/server/services/nausys'
 import { geocoder } from '../../../../src/server/services/arcgis'
+import { geoEncode } from '../../../../src/shared/utils/geohash'
 const { 
 	DEFAULT_LOCALE
 } = process.env
@@ -23,19 +24,25 @@ test('retrieve locations from nausys api with a non empty array', async () => {
 	const { locations } = data
 	for(let i=0;i<locations.length;i++){
 		const place = locations[i]
-		const { name, lat, lon } = place 
+		let latitude, longitude
+		const { name, lat, lon, id, regionId } = place 
 		const reverseOnce = await geocoder({ location: `${lon}, ${lat}` }, 'reverse')
 		const text = name[`text${DEFAULT_LOCALE}`]
 		let { address } = reverseOnce
+		latitude = lat
+		longitude = lon
 		if(address.Type === 'Sea'){
 			const geo = await geocoder({ text }, 'location')
 			const [{ extent, feature }] = geo.locations
 			const { geometry, attributes } = feature 
 			const { x, y } = geometry
 			const reverseAgain = await geocoder({ location: `${x}, ${y}` }, 'reverse')
+			latitude = y
+			longitude = x
 			address = reverseAgain.address
 		}
-		console.log(address, text)
+		const geohash = geoEncode(latitude, longitude, 6)
+		console.log(address, text, regionId, geohash)
 		expect(address).not.toBe(undefined)
 	}
 }, 100000)
