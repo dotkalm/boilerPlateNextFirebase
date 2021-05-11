d77454bd194987e0edf8a9ba8b668a5777cf2507
joel holmberg <joelmholmberg@gmail.com>
2021-05-10 22:08:04 -0700

    shared secret

--- src/pages/api/graphql.js
+++ src/pages/api/graphql.js
@@ -8,8 +8,7 @@ const server = async (request, response, graphQLParams) => {
 		rootValue: (hmac, query) => {
 			const truth = verifyHmac(hmac, query)
 			if(!truth){
-				response.sendError("message goes here")
-
+				response.sendError({status: 100, message: "invalid hmac"})
 			}else{
 				return truth
 			}
